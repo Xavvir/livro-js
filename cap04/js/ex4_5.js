89b89cecb38158846409c65c26bd11e7c1090d98
Xavvir <leandroxm1997@hotmail.com>
2024-07-16 23:01:19 -0300

    programa pescaria

--- cap04/js/ex4_5.js
+++ cap04/js/ex4_5.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', (e) => {
   const numero = Number(form.inNumero.value)
   const raiz = Math.sqrt(numero)
   
-  if (Number.isInteger(raiz)){
+  if (Number.isInteger(raiz)){ // verifica se o valor da raiz é um número inteiro
     resp.innerText = `Raiz: ${raiz}`
   } else {
     resp.innerText = `Não há raiz exata para o número ${numero}.`
