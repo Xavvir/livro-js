f165826dfdeffe096aa15d7442b4eaa12b655c50
Xavvir <leandroxm1997@hotmail.com>
2024-09-15 19:09:57 -0300

    finaalização revenda carro

--- cap06/js/ex6_3.js
+++ cap06/js/ex6_3.js
@@ -22,13 +22,48 @@ form.btListar.addEventListener("click", () => {
   }
 
   const lista = carros.reduce(
-    (acumulador, carro) => acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n" , "");
+    (acumulador, carro) => acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "");
   resp.innerText = `Lista dos Carros Cadastrados \n${"-".repeat(40)} \n ${lista}`
 });
 
 form.btFiltrar.addEventListener("click", () => {
-  let maximo = Number(prompt("Digite o valor máximo:"))
+  const maximo = Number(prompt("Digite o valor máximo:"))
+  const maximobr = maximo.toLocaleString('pt-br', { style: "currency", currency: 'BRL' })
+  if (maximo == 0 | isNaN(maximo)) {
+    alert("Digite um valor acima de 0")
+    return
+  }
 
   const filtro = carros.filter(carro => carro.preco <= maximo)
-  resp.innerText = `Modelo: ${filtro.modelo} Preço: ${filtro.preco} `
+  if (filtro.length == 0) {
+    alert("Não há carros com preço inferior ao informado.")
+    return
+  }
+
+  let lista = ""
+
+  for (const carro of filtro) {    // percorre cad elemento do array
+    lista += `${carro.modelo} - ${carro.preco.toFixed(2)}\n`
+  }
+
+  resp.innerText = `Carros até ${maximobr}\n${lista}`
+})
+
+form.btSimular.addEventListener("click", () => {
+  const desconto = Number(prompt("Digite o % de desconto: "))
+  if (desconto == 0 | isNaN(desconto)) {
+    return
+  }
+
+  const carrosDesc = carros.map(aux => ({
+    modelo: aux.modelo,
+    preco: aux.preco - (aux.preco * desconto / 100)
+  }))
+
+  let lista = ""
+  for (const carro of carrosDesc) {
+    lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`
+  }
+
+  resp.innerText = `Carros com desconto: ${desconto}%\n${"-".repeat(40)}\n${lista}`
 })
